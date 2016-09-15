44b316cf5caebb2b1b266ca4c06227e435e3b802
Sanil Hattangadi <456570@beaverton.k12.or.us>
2016-09-15 14:27:57 -0700

    fixed the bugs and asked to play again

--- guessinggame.cpp
+++ guessinggame.cpp
@@ -1,25 +1,39 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <cstring>
 using namespace std;
 
+
 int main() {
-  int randomNum = rand()*101
+  srand (time(NULL));
+  int randomNum = rand()%101;
   int guess = -1;
-  int count = 0;
+  int count = 1;
   bool play = true;
-  cout << "Welcome to Guessing Game, where you will have to guess the number I'm thinking of."<< endl;
-  cout << "The number is between 0 and 100. Guess what number it is?"<< endl;
+  char yesno[4];
+  cout << "Welcome to Guessing Game, where you will have to guess the number I'm thinking of." << endl;
+  cout << "The number is between 0 and 100.Guess what number it is?" << endl;
   while (play == true){
-    cin>> guess;
-    if (guess << randomNum){
-      cout << "The guess is too low. Guess again."<<endl;
-      count+=1;
+    cin >> guess;
+    if (guess < randomNum){
+      cout << "The guess is too low. Guess again."<< endl;
+      count++;
     }
-    if (guess >> randomNum){
-      cout << "The guess it too high"<<endl;
-      count+=1;
+    else if (guess > randomNum){
+      cout << "The guess it too high. Guess again"<< endl;
+      count++;
     }
-    if (guess == randomNum) {
-      cout<< "You won! It took you" + guess + "guesses"<< end;
+    else if (guess == randomNum){
+      cout << "You won! It took you " << count << " guesses"<< endl;
+      play = false; 
+      cout << "Do you want to play again?" << endl;
+      cin>> yesno;
+      if (0 == strcmp(yesno, "y")){
+	play = true;
+	randomNum = rand()%101;
+      }
     }
-  return 0;
-}
   }
+}
+//random numbers are the same each time
